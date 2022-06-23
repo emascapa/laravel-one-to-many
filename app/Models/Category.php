ab1811b01ac3f720fb6d96c2ef6234f512639295
emascapa <emascapa@gmail.com>
2022-06-23 16:07:57 +0200

    Add relation one to many to classes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class Category extends Model
 {
-    //
+    //protected $fillable = ['title', 'slug', 'image', 'content', 'date'];
+
+
+    public function posts(): HasMany 
+    {
+        # code...
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Post extends Model
 {
     //
     protected $fillable = ['title', 'slug', 'image', 'content', 'date'];
 
+    public function category(): BelongsTo
+    {
+        # code...
+        return $this->belongsTo(Category::class);
+    }
+    
 }
